d2ebb0a5357eebbb5799c153fbeebaeb3aa20c1f
kuangrenjing <mqs198506@hotmail.com>
2019-04-16 17:30:20 +0800

    2019-04-16
    python

---
+++ language/python/test/0-100/001.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+#print函数的使用
+print('hello world')
+#print("today is ", '10')
+stooge = 'hailen'
+print(stooge, "says: good night")
---
+++ language/python/test/0-100/002.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+#旧式格式化方法
+s1="today is %d-%02d-%d"%(2019, 4, 16)
+print(s1)
+
+s2='"today is %d-%02d-%d" % (2019, 4, 16)'
+print(s2)
