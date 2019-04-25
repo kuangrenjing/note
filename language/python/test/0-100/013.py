8218d1cc00eda8110b56493a95e63bbbc6d51105
kuangrenjing <mqs198506@hotmail.com>
2019-04-25 18:01:41 +0800

    2019-04-25
    python study2019-04-25
    
    python study2019-04-25
    
    python study2019-04-25
    
    python study2019-04-25
    
    python study2019-04-25
    
    python study2019-04-25
    
    python study2019-04-25
    
    python study2019-04-25
    
    python study2019-04-25
    
    python study2019-04-25
    
    python study2019-04-25
    
    python study2019-04-25
    
    python study2019-04-25
    
    python study2019-04-25
    
    python study2019-04-25
    
    python study2019-04-25
    
    python study2019-04-25
    
    python study2019-04-25
    
    python study2019-04-25
    
    python study

--- language/python/test/0-100/013.py
+++ language/python/test/0-100/013.py
@@ -5,10 +5,26 @@
 
 class CPerson():
 	a = 8
+	kind = "black"
+	def __init__(self, name):
+		self.name =name
 
-person = CPerson()
+	def print_name(self):
+		print(self.name)
+
+	@classmethod
+	def print_kind(cls):
+		print(CPerson.kind)
+
+person = CPerson('mqs')
 
 
-print("CPerson's type is ", type(CPerson))
-print("person's type is ", type(person))
-print("person's a is ", person.a)
+#print("CPerson's type is ", type(CPerson))
+#print("person's type is ", type(person))
+#print("person's a is ", person.a)
+
+print(type(person.print_name))
+print(type(person.print_kind))
+print(type(CPerson.print_kind))
+
+CPerson.print_kind()
