47f9aa02076bc4c75163018796f032d4f563e209
mqs <mqs198506@hotmail.com>
2019-05-05 12:57:37 +0800

    2019-05-05
    c++ study

---
+++ language/C++/test/RTTI/RTTI/RTTI.cpp
@@ -0,0 +1,50 @@
+﻿// RTTI.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include "pch.h"
+#include <iostream>
+
+#include "iostream"   
+using namespace std;
+
+
+class A{
+	public:virtual void foo()
+	{
+		cout << "A:foo()\n";
+	}
+
+	void pp()
+	{
+		cout << "A:pp()\n";
+	}
+};
+class B :public A
+{
+public:
+	void foo()
+	{
+		cout << "B:foo()\n";
+	}
+	void pp()
+	{
+		cout << "B:pp()\n";
+	}
+	void fun()
+	{
+		cout << "function in B \n";
+	}
+
+
+};
+void main()
+{
+	B b;
+	A *pa = &b;
+	pa->foo();//由于存在多态，所以根据指向的类型调用方法，否则根据指针类型调用方法   
+	pa->pp();//不存在多态，根据指针类型调用   
+	//pa->fun();//出错，无法调用   
+	dynamic_cast<B*>(pa)->pp();//运行时动态类型转换，可以指向B的pp（）   
+	dynamic_cast<B*>(pa)->fun();//运行时动态类型转换，可以指向B的fun（）   
+	dynamic_cast<B*>(pa)->foo();//运行时动态类型转换，可以指向B的foo（）   
+}
---
+++ language/C++/test/typeid/typeid/typeid.cpp
@@ -0,0 +1,70 @@
+﻿// typeid.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include "pch.h"
+#include <iostream>
+
+using namespace std;
+
+class X
+{
+public:
+	X()
+	{
+		mX = 101;
+	}
+	virtual void vfunc()
+	{
+		cout << "X::vfunc()" << endl;
+	}
+private:
+	int mX;
+};
+
+class XX : public X
+{
+public:
+	XX():X()
+	{
+		mXX = 1001;
+	}
+	virtual void vfunc()
+	{
+		cout << "XX::vfunc()" << endl;
+	}
+private:
+	int mXX;
+};
+
+class Y
+{
+public:
+	Y()
+	{
+		mY = 101;
+	}
+	
+private:
+	int mY;
+};
+
+int main()
+{
+	int n = 0;
+	XX xx;
+	Y y;
+	Y *py = &y;
+
+	// int和XX都是类型名
+	cout << typeid(int).name() << endl;
+	cout << typeid(XX).name() << endl;
+	// n为基本变量
+	cout << typeid(n).name() << endl;
+	// xx所属的类虽然存在virtual，但是xx为一个具体的对象
+	cout << typeid(xx).name() << endl;
+	// py为一个指针，属于基本类型
+	cout << typeid(py).name() << endl;
+	// py指向的Y的对象，但是类Y不存在virtual函数
+	cout << typeid(*py).name() << endl;
+	return 0;
+}
