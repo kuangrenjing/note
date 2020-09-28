af63d50f1ec0e8eff0c129a799e6cd641a145709
mqs <mqs198506@hotmail.com>
2020-09-28 19:03:41 +0800

    feat(git):some new
    以上

--- language/python/test/import_test/hello_who.py
+++ language/python/test/import_test/hello_who.py
@@ -1,17 +1,7 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
 
-#who = "mqs"
-
-import sys
-import os
-#sys.path.append(os.path.dirname(__file__))
-print("[hello_who]sys.path is ", sys.path)
-
-#import import_test.who
-from import_test import who
-
-print("[hello_who]__name__ is ", __name__)
+who = "mqs"
 
 def hello_who_print():
-	print("hello", who.who)
+	print("hello", who)
--- language/python/test/main.py
+++ language/python/test/main.py
@@ -7,13 +7,18 @@ import os
 #sys.path.append(os.path.dirname(__file__))
 
 print("sys.path is ", sys.path)
-
+print("__name__ is ", __name__)
 #import import_test.hello_who
-from import_test import hello_who
+from import_test.hello_who import hello_who_print
 
-print("hello_who.__file__ is ", hello_who.__file__)
+
+print("dir(hello_who_print) is ", dir(hello_who_print))
+
+print("hello_who_print.__module__ is ", hello_who_print.__module__)
+
+#print("hello_who.__file__ is ", hello_who.__file__)
 
 #print("__file__ is ", __file__)
 #print("os.path.dirname(__file__) is ", os.path.dirname(__file__))
 
-hello_who.hello_who_print()
+hello_who_print()
